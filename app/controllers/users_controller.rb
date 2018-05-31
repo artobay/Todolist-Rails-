2dd5bf6d65022018498e0c4bd33ff7964302d11d
Aliyar F <aliyarfatemi@gmail.com>
2018-05-31 00:18:45 +0000

    revision for user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,16 +15,23 @@ class UsersController < ApplicationController
    if @user.save
      flash[:notice] = "Welcome to Blocitoff #{@user.name}"
      create_session(@user)
-      redirect_to root path
+      redirect_to root_path
     else
       flash.now[:alert] = "There was an error creating you account. Please try again."
       render :new
    end
   end
 
-  def show
-       @item = Item.new
-  end
+   def show
+    if current_user.present?
+      @user = current_user
+      @item = Item.new
+      @items = @user.items
+    else
+      redirect_to new_user_registration_path
+    end  
+   end
+
 
   def destroy
   end
